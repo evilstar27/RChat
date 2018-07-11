c7dfd59147c39ecc9b6f813666edb90683df68e0
SillyRen <rsx_game@163.com>
2018-07-11 23:44:44 +0800

    封装Activity和Fragment -1

---
+++ common/src/main/java/com/test/morningstar/common/app/Fragment.java
@@ -0,0 +1,80 @@
+package com.test.morningstar.common.app;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author morningstar
+ * @date 2018/7/11
+ */
+public abstract class Fragment extends android.support.v4.app.Fragment {
+    protected View mRoot;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // 初始化参数
+        initArgs(getArguments());
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        if (mRoot == null) {
+            int layId = getContentLayoutId();
+            // 初始化当前的根布局，但是不在创建时就添加到container里边
+            View root = inflater.inflate(layId, container, false);
+            initWidget(root);
+            mRoot = root;
+        } else {
+            if (mRoot.getParent() != null) {
+                // 把当前Root从其父控件中移除
+                ((ViewGroup) mRoot.getParent()).removeView(mRoot);
+            }
+        }
+
+        return mRoot;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // 当View创建完成后初始化数据
+        initData();
+    }
+
+    /**
+     * 得到当前界面的资源文件Id
+     *
+     * @return 资源文件Id
+     */
+    protected abstract int getContentLayoutId();
+
+    /**
+     * 初始化控件
+     */
+    protected void initWidget(View root) {
+
+    }
+
+    /**
+     * 初始化数据
+     */
+    protected void initData() {
+
+    }
+
+    /**
+     * 初始化相关参数
+     */
+    protected void initArgs(Bundle bundle){
+
+    }
+}
