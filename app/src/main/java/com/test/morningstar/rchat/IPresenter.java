a15bd81ca9d9e78176dc6c493919db58e40f3a19
SillyRen <rsx_game@163.com>
2018-07-30 23:56:31 +0800

    导入资源文件

--- app/src/main/java/com/test/morningstar/rchat/IPresenter.java
+++
@@ -1,9 +0,0 @@
-package com.test.morningstar.rchat;
-
-/**
- * @author morningstar
- * @date 2018/7/25
- */
-public interface IPresenter {
-    void search();
-}
--- app/src/main/java/com/test/morningstar/rchat/MainActivity.java
+++ app/src/main/java/com/test/morningstar/rchat/MainActivity.java
@@ -1,21 +1,8 @@
 package com.test.morningstar.rchat;
 
-import android.widget.EditText;
-import android.widget.TextView;
-
 import com.test.morningstar.common.app.Activity;
 
-import butterknife.BindView;
-import butterknife.OnClick;
-
-public class MainActivity extends Activity implements IView{
-    @BindView(R.id.text_test)
-    TextView mTestText;
-
-    @BindView(R.id.edit_search)
-    EditText mSearchText;
-
-    private IPresenter mPresenter;
+public class MainActivity extends Activity {
 
     @Override
     protected int getContentLayoutId() {
@@ -25,22 +12,6 @@ public class MainActivity extends Activity implements IView{
     @Override
     protected void initData() {
         super.initData();
-        mPresenter = new Presenter(this);
     }
 
-    @OnClick(R.id.button_submit)
-    void onSubmit(){
-        mPresenter.search();
-    }
-
-
-    @Override
-    public String getInputString() {
-        return mSearchText.getText().toString();
-    }
-
-    @Override
-    public void setResultString(String string) {
-        mTestText.setText(string);
-    }
 }
